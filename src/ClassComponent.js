3d5ad60687287e42c5e00119b9fe0236c69df0bb
pbsc <pbsc@csclab1sys122.parjanya.pbsiddhartha.ac.in>
2022-11-30 16:51:28 +0530

    react components

---
+++ src/ClassComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>
+			This is Class about Component
+			</div>
+			)
+	}
+}
+
+export default ClassComponent;
